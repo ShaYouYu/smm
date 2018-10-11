a138bc769c0b69ec9400a67b4cdf123ae3be5d36
ShaYouYu <994302201@qq.com>
2018-10-11 20:38:44 +0800

    first commit

---
+++ src/main/java/com/shsxt/service/UserService.java
@@ -0,0 +1,17 @@
+package com.shsxt.service;
+
+import com.shsxt.base.BaseService;
+import com.shsxt.dao.UserMapper;
+import com.shsxt.po.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by xlf on 2018/10/11.
+ */
+@Service
+public class UserService extends BaseService<User>{
+
+    @Autowired
+    private UserMapper userMapper;
+}
